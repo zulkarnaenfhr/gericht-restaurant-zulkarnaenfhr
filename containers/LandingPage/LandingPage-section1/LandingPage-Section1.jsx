3aef09f73476d6778997770ecec337831b0a3ba4
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2023-02-01 01:06:24 +0700

    add awesome and section left

--- containers/LandingPage/LandingPage-section1/LandingPage-Section1.jsx
+++ containers/LandingPage/LandingPage-section1/LandingPage-Section1.jsx
@@ -1,10 +1,37 @@
+import Image from "next/image";
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import styles from "./LandingPage-Section1.module.css";
 
 const LandingPageSection1 = () => {
     return (
-        <div>
-            <h1>masasdasdasdddduk</h1>
+        <div id={styles["LandingPage-Section1"]}>
+            <div className={styles["LandingPage-Section1-Container"]}>
+                <Row>
+                    <Col className={styles["LandingPage-Section1-Column-Left"]}>
+                        <div className={styles["LandingPage-Section1-Column-Left-Container"]}>
+                            <p className={styles["LandingPage-Section1-Text1"]}>Chase the new Flavour</p>
+
+                            <Image
+                                style={{
+                                    marginTop: "8px",
+                                }}
+                                src={require("../../../src/icon/yellow-spoon.svg")}
+                            />
+
+                            <p className={styles["LandingPage-Section1-Text2"]}>
+                                The key to <br /> Fine dining
+                            </p>
+
+                            <p className={styles["LandingPage-Section1-Text3"]}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                            <button className={styles["LandingPage-Section1-Button"]}>Expolre Menu</button>
+                        </div>
+                    </Col>
+                    <Col>
+                        <h1>kanan</h1>
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 };
