755c50f5c0a01f2e7a1f0670eb54efa195018ac4
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2023-01-31 23:38:08 +0700

    initial commit

---
+++ components/NavbarGericht/NavbarGericht.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "./NavbarGericht.module.css";
+
+export default function NavbarGericht() {
+    const [innerWidth, setInnerWidth] = useState(0);
+
+    useEffect(() => {
+        setInnerWidth(window.innerWidth);
+    }, []);
+
+    return (
+        <div id={styles["NavbarGericht"]}>
+            <Navbar className={styles["NavbarGericht-Container"]} expand="lg" variant="dark">
+                <Navbar.Brand className={styles["NavbarGericht-Brand"]} href="#home">
+                    Gerícht
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse
+                    id="basic-navbar-nav"
+                    style={{
+                        position: "relative",
+                    }}
+                >
+                    <Nav className="ms-auto">
+                        {innerWidth < 992 ? (
+                            <div>
+                                <Nav.Link className={styles["NavbarGericht-Link"]} href="#link">
+                                    Home
+                                </Nav.Link>{" "}
+                                <Nav.Link className={styles["NavbarGericht-Link"]} href="#link">
+                                    Pages
+                                </Nav.Link>{" "}
+                                <Nav.Link className={styles["NavbarGericht-Link"]} href="#link">
+                                    Contact Us
+                                </Nav.Link>{" "}
+                                <Nav.Link className={styles["NavbarGericht-Link"]} href="#link">
+                                    Blog{" "}
+                                </Nav.Link>{" "}
+                                <Nav.Link className={styles["NavbarGericht-Link"]} href="#link">
+                                    Landing{" "}
+                                </Nav.Link>{" "}
+                            </div>
+                        ) : (
+                            ""
+                        )}
+
+                        <Nav.Link className={styles["NavbarGericht-Link"]} href="#home">
+                            Log In / Registration
+                        </Nav.Link>
+
+                        {innerWidth > 992 ? (
+                            <div
+                                style={{
+                                    paddingRight: "10px",
+                                    borderRight: "0.2px solid white",
+                                    marginRight: "10px",
+                                }}
+                            ></div>
+                        ) : (
+                            ""
+                        )}
+
+                        <Nav.Link className={styles["NavbarGericht-Link"]} href="#link">
+                            Book Table
+                        </Nav.Link>
+                    </Nav>
+                    {innerWidth > 992 ? (
+                        <div className={styles["NavbarGericht-ColumnCenter"]}>
+                            <Nav.Link className={styles["NavbarGericht-LinkCenter"]} href="#link">
+                                Home
+                            </Nav.Link>{" "}
+                            <Nav.Link className={styles["NavbarGericht-LinkCenter"]} href="#link">
+                                Pages
+                            </Nav.Link>{" "}
+                            <Nav.Link className={styles["NavbarGericht-LinkCenter"]} href="#link">
+                                Contact Us
+                            </Nav.Link>{" "}
+                            <Nav.Link className={styles["NavbarGericht-LinkCenter"]} href="#link">
+                                Blog{" "}
+                            </Nav.Link>{" "}
+                            <Nav.Link className={styles["NavbarGericht-LinkCenter"]} href="#link">
+                                Landing{" "}
+                            </Nav.Link>{" "}
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                </Navbar.Collapse>
+            </Navbar>{" "}
+        </div>
+    );
+}
---
+++ containers/LandingPage/LandingPage-section1/LandingPage-Section1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./LandingPage-Section1.module.css";
+
+const LandingPageSection1 = () => {
+    return (
+        <div>
+            <h1>masasdasdasdddduk</h1>
+        </div>
+    );
+};
+
+export default LandingPageSection1;
