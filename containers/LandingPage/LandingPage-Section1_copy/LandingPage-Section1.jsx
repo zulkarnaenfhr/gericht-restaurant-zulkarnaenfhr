02bd14000db00f73ceca5f36238c00465c5803cf
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2023-02-06 22:21:23 +0700

    responsive mobile section 1

--- containers/LandingPage/LandingPage-Section1_copy/LandingPage-Section1.jsx
+++ containers/LandingPage/LandingPage-Section1_copy/LandingPage-Section1.jsx
@@ -7,43 +7,48 @@ const LandingPageSection1 = () => {
     return (
         <div id={styles["LandingPage-Section1"]}>
             <Row className={styles["LandingPage-Section1-Container"]}>
-                <Col className={styles["LandingPage-Section1-Column-Left"]}>
-                    <div className={styles["LandingPage-Section1-Column-Left-Container"]}>
-                        <p className={styles["LandingPage-Section1-Text1"]}>Chase the new Flavour</p>
+                <Col md={6}>
+                    <div className={styles["LandingPage-Section1-Column-Left"]}>
+                        <div className={styles["LandingPage-Section1-Column-Left-Container"]}>
+                            <p className={styles["LandingPage-Section1-Text1"]}>Chase the new Flavour</p>
 
-                        <Image
-                            style={{
-                                marginTop: "8px",
-                            }}
-                            src={require("../../../src/icon/yellow-spoon.svg")}
-                        />
+                            <Image
+                                style={{
+                                    marginTop: "8px",
+                                }}
+                                src={require("../../../src/icon/yellow-spoon.svg")}
+                            />
 
-                        <p className={styles["LandingPage-Section1-Text2"]}>The key to Fine dining</p>
+                            <p className={styles["LandingPage-Section1-Text2"]}>The key to Fine dining</p>
 
-                        <p className={styles["LandingPage-Section1-Text3"]}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-                        <button className={styles["LandingPage-Section1-Button"]}>Expolre Menu</button>
+                            <p className={styles["LandingPage-Section1-Text3"]}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                            <button className={styles["LandingPage-Section1-Button"]}>Expolre Menu</button>
+                        </div>
                     </div>
                 </Col>
-                <Col className={styles["LandingPage-Section1-Column-Right"]}>
-                    <div className={styles["LandingPage-Section1-Column-Right-Container"]}>
-                        <div
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                            }}
-                        >
-                            <div>
-                                <div className={styles["LandingPage-Section1-Column-Right-Box1"]}></div>
-                                <div className={styles["LandingPage-Section1-Column-Right-Box2"]}></div>
-                            </div>
-                            <div className={styles["LandingPage-Section1-Column-Pictures-Container-Parents"]}>
-                                <div className={styles["LandingPage-Section1-Column-Pictures-Container"]}>
-                                    <Image className={styles["LandingPage-Section1-Column-Pictures"]} src={require("../../../public/images/Section1.png")} />
+                <Col md={6}>
+                    <div className={styles["LandingPage-Section1-Column-Right"]}>
+                        <div className={styles["LandingPage-Section1-Column-Right-Container"]}>
+                            <div
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            >
+                                <div>
+                                    <div className={styles["LandingPage-Section1-Column-Right-Box1"]}></div>
+                                    <div className={styles["LandingPage-Section1-Column-Right-Box2"]}></div>
+                                </div>
+                                <div className={styles["LandingPage-Section1-Column-Pictures-Container-Parents"]}>
+                                    <div className={styles["LandingPage-Section1-Column-Pictures-Container"]}>
+                                        <Image className={styles["LandingPage-Section1-Column-Pictures"]} src={require("../../../public/images/Section1.png")} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </Col>
+                <button className={styles["LandingPage-Section1-ButtonCopy"]}>Expolre Menu</button>
             </Row>
         </div>
     );
