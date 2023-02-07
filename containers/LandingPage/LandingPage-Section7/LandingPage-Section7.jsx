fa2005857d2a04eee3d7b381356dc588d47864cf
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2023-02-08 02:20:59 +0700

    cek

--- containers/LandingPage/LandingPage-Section7/LandingPage-Section7.jsx
+++ containers/LandingPage/LandingPage-Section7/LandingPage-Section7.jsx
@@ -19,39 +19,43 @@ const LandingPageSection7 = () => {
                 <Image className={styles["LandingPage-Section7-Background"]} src={require("../../../public/images/Section2-Background.png")} />
             </div>
             <Row className={styles["LandingPage-Section7-Container"]}>
-                <Col md={6} className={styles["LandingPage-Section7-Column"]}>
-                    <div>
-                        <p className={styles["LandingPage-Section7-Text1"]}>Contact</p>
-                        <Image
-                            style={{
-                                marginTop: "8px",
-                                transform: "rotate(180deg)",
-                            }}
-                            src={require("../../../src/icon/yellow-spoon.svg")}
-                        />
-                        <p className={styles["LandingPage-Section7-Text2"]}>Find Us</p>
-                        <p className={styles["LandingPage-Section7-Text3"]}>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
-                        <p className={styles["LandingPage-Section7-Text4"]}>Opening Hours</p>
-                        <p className={styles["LandingPage-Section7-Text5"]}>Mon - Fri: 10:00 am - 02:00 am</p>
-                        <p className={styles["LandingPage-Section7-Text6"]}>Sat - Sun: 10:00 am - 03:00 am</p>
-                        <button className={styles["LandingPage-Section7-Button"]}>Visit Us</button>
+                <Col md={6}>
+                    <div className={styles["LandingPage-Section7-Column"]}>
+                        <div>
+                            <p className={styles["LandingPage-Section7-Text1"]}>Contact</p>
+                            <Image
+                                style={{
+                                    marginTop: "8px",
+                                    transform: "rotate(180deg)",
+                                }}
+                                src={require("../../../src/icon/yellow-spoon.svg")}
+                            />
+                            <p className={styles["LandingPage-Section7-Text2"]}>Find Us</p>
+                            <p className={styles["LandingPage-Section7-Text3"]}>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+                            <p className={styles["LandingPage-Section7-Text4"]}>Opening Hours</p>
+                            <p className={styles["LandingPage-Section7-Text5"]}>Mon - Fri: 10:00 am - 02:00 am</p>
+                            <p className={styles["LandingPage-Section7-Text6"]}>Sat - Sun: 10:00 am - 03:00 am</p>
+                            <button className={styles["LandingPage-Section7-Button"]}>Visit Us</button>
+                        </div>
                     </div>
                 </Col>
-                <Col md={6} className={styles["LandingPage-Section1-Column-Right"]}>
-                    <div className={styles["LandingPage-Section1-Column-Right-Container"]}>
-                        <div
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                            }}
-                        >
-                            <div>
-                                <div className={styles["LandingPage-Section1-Column-Right-Box1"]}></div>
-                                <div className={styles["LandingPage-Section1-Column-Right-Box2"]}></div>
-                            </div>
-                            <div className={styles["LandingPage-Section1-Column-Pictures-Container-Parents"]}>
-                                <div className={styles["LandingPage-Section1-Column-Pictures-Container"]}>
-                                    <Image className={styles["LandingPage-Section1-Column-Pictures"]} src={require("../../../public/images/Section7-Image.png")} />
+                <Col md={6}>
+                    <div className={styles["LandingPage-Section1-Column-Right"]}>
+                        <div className={styles["LandingPage-Section1-Column-Right-Container"]}>
+                            <div
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            >
+                                <div>
+                                    <div className={styles["LandingPage-Section1-Column-Right-Box1"]}></div>
+                                    <div className={styles["LandingPage-Section1-Column-Right-Box2"]}></div>
+                                </div>
+                                <div className={styles["LandingPage-Section1-Column-Pictures-Container-Parents"]}>
+                                    <div className={styles["LandingPage-Section1-Column-Pictures-Container"]}>
+                                        <Image className={styles["LandingPage-Section1-Column-Pictures"]} src={require("../../../public/images/Section7-Image.png")} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
