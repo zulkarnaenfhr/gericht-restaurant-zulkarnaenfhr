e1b4bb217fa132f24746bddf0880448f0f24d6dd
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2023-02-09 12:24:09 +0700

    section 4 dan 5

--- containers/LandingPage/LandingPage-Section4/LandingPage-Section4.jsx
+++ containers/LandingPage/LandingPage-Section4/LandingPage-Section4.jsx
@@ -1,9 +1,55 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import styles from "./LandingPage-Section4.module.css";
 
+import gsap from "gsap/dist/gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 const LandingPageSection4 = () => {
+    let useEffectCall = 0;
+
+    const refImageContainer = useRef();
+    const refTextContainer = useRef();
+
+    useEffect(() => {
+        if (useEffectCall > 0) return;
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(refImageContainer.current, 0, { yPercent: -10 });
+        gsap.to(refTextContainer.current, 0, { yPercent: -5 });
+
+        setTimeout(() => {
+            setUpAnimations();
+        }, 1000);
+
+        useEffectCall++;
+    }, []);
+
+    const setUpAnimations = () => {
+        gsap.to(refImageContainer.current, {
+            yPercent: 0,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: refImageContainer.current,
+                start: "center 100%",
+                end: "center 50%",
+                scrub: 1,
+            },
+        });
+
+        gsap.to(refTextContainer.current, {
+            yPercent: 0,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: refTextContainer.current,
+                start: "center 100%",
+                end: "center 50%",
+                scrub: 1,
+            },
+        });
+    };
+
     return (
         <div id={styles["LandingPage-Section4"]}>
             <div className={styles["LandingPage-Section4-Background-Container"]}>
@@ -19,7 +65,7 @@ const LandingPageSection4 = () => {
                 <Image className={styles["LandingPage-Section4-Background"]} src={require("../../../public/images/Section2-Background.png")} />
             </div>
             <Row className={styles["LandingPage-Section4-Container"]}>
-                <Col md={6} className={styles["LandingPage-Section4-Column"]}>
+                <Col ref={refImageContainer} md={6} className={styles["LandingPage-Section4-Column"]}>
                     <div className={styles["LandingPage-Section4-Left-Container"]}>
                         <div
                             style={{
@@ -40,6 +86,7 @@ const LandingPageSection4 = () => {
                     </div>
                 </Col>
                 <Col
+                    ref={refTextContainer}
                     md={6}
                     className={styles["LandingPage-Section4-Column"]}
                     style={{
@@ -51,22 +98,31 @@ const LandingPageSection4 = () => {
                             maxWidth: "480px",
                         }}
                     >
-                        <p className={styles["LandingPage-Section4-Text1"]}>Chef’s Word</p>
+                        <p data-aos="zoom-in-right" className={styles["LandingPage-Section4-Text1"]}>
+                            Chef’s Word
+                        </p>
                         <Image
+                            data-aos="zoom-in-right"
                             style={{
                                 marginTop: "8px",
                                 transform: "rotate(180deg)",
                             }}
                             src={require("../../../src/icon/yellow-spoon.svg")}
                         />
-                        <p className={styles["LandingPage-Section4-Text2"]}>What we believe in</p>
-                        <p className={styles["LandingPage-Section4-Text3"]}>
+                        <p data-aos="zoom-in-right" className={styles["LandingPage-Section4-Text2"]}>
+                            What we believe in
+                        </p>
+                        <p data-aos="zoom-in-right" className={styles["LandingPage-Section4-Text3"]}>
                             "Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit. auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
                             lectus eu. Congue iaculis integer curabitur semper sit nunc."
                         </p>
-                        <p className={styles["LandingPage-Section4-Text4"]}>Kevin Luo</p>
-                        <p className={styles["LandingPage-Section4-Text5"]}>Chef & Founder</p>
-                        <div className={styles["LandingPage-Section4-Signiature-Container"]}>
+                        <p data-aos="zoom-in-right" className={styles["LandingPage-Section4-Text4"]}>
+                            Kevin Luo
+                        </p>
+                        <p data-aos="zoom-in-right" className={styles["LandingPage-Section4-Text5"]}>
+                            Chef & Founder
+                        </p>
+                        <div data-aos="zoom-in-right" className={styles["LandingPage-Section4-Signiature-Container"]}>
                             <Image className={styles["LandingPage-Section4-Signiature"]} src={require("../../../public/images/Section4-Signiature.png")} />
                         </div>
                     </div>
--- containers/LandingPage/LandingPage-Section5/LandingPage-Section5.jsx
+++ containers/LandingPage/LandingPage-Section5/LandingPage-Section5.jsx
@@ -1,26 +1,64 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import styles from "./LandingPage-Section5.module.css";
 
+import gsap from "gsap/dist/gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 const LandingPageSection5 = () => {
+    let useEffectCall = 0;
+
+    const refImageContainer = useRef();
+
+    useEffect(() => {
+        if (useEffectCall > 0) return;
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(refImageContainer.current, 0, { yPercent: 10 });
+
+        setTimeout(() => {
+            setUpAnimations();
+        }, 1000);
+
+        useEffectCall++;
+    }, []);
+
+    const setUpAnimations = () => {
+        gsap.to(refImageContainer.current, {
+            yPercent: 0,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: refImageContainer.current,
+                start: "center 100%",
+                end: "center 50%",
+                scrub: 1,
+            },
+        });
+    };
+
     return (
         <div id={styles["LandingPage-Section5"]}>
             <div className={styles["LandingPage-Section5-Container"]}>
                 <Row>
                     <Col className={styles["LandingPage-Section5-Column"]} md={6} lg={7}>
                         <div className={styles["LandingPage-Section5-Column-Left-Container"]}>
-                            <p className={styles["LandingPage-Section5-text1"]}>Awards & recognition</p>
+                            <p data-aos="zoom-out-up" className={styles["LandingPage-Section5-text1"]}>
+                                Awards & recognition
+                            </p>
                             <Image
+                                data-aos="zoom-out-up"
                                 style={{
                                     marginTop: "8px",
                                 }}
                                 src={require("../../../src/icon/yellow-spoon.svg")}
                             />
-                            <p className={styles["LandingPage-Section5-text2"]}>Our Laurels</p>
+                            <p data-aos="zoom-out-up" className={styles["LandingPage-Section5-text2"]}>
+                                Our Laurels
+                            </p>
                             <Row className={styles["LandingPage-Section5-Column-Little-Row"]}>
                                 <Col className={styles["LandingPage-Section5-Column-Little"]}>
-                                    <div className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
+                                    <div data-aos="zoom-out-up" className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
                                         <Image className={styles["LandingPage-Section5-Column-Little-Icon"]} src={require("../../../public/images/Section5-No2.png")} />
                                     </div>
                                     <div
@@ -28,12 +66,16 @@ const LandingPageSection5 = () => {
                                             paddingLeft: "22px",
                                         }}
                                     >
-                                        <p className={styles["LandingPage-Section5-Column-Little-Title"]}>Bib Gourmond</p>
-                                        <p className={styles["LandingPage-Section5-Column-Little-Desc"]}>Lorem ipsum dolor sit amet, consectetur.</p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="200" className={styles["LandingPage-Section5-Column-Little-Title"]}>
+                                            Bib Gourmond
+                                        </p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="400" className={styles["LandingPage-Section5-Column-Little-Desc"]}>
+                                            Lorem ipsum dolor sit amet, consectetur.
+                                        </p>
                                     </div>
                                 </Col>
                                 <Col className={styles["LandingPage-Section5-Column-Little"]}>
-                                    <div className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
+                                    <div data-aos="zoom-out-up" className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
                                         <Image className={styles["LandingPage-Section5-Column-Little-Icon"]} src={require("../../../public/images/Section5-No1.png")} />
                                     </div>
                                     <div
@@ -41,14 +83,18 @@ const LandingPageSection5 = () => {
                                             paddingLeft: "22px",
                                         }}
                                     >
-                                        <p className={styles["LandingPage-Section5-Column-Little-Title"]}>Rising Star</p>
-                                        <p className={styles["LandingPage-Section5-Column-Little-Desc"]}>Lorem ipsum dolor sit amet, consectetur.</p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="200" className={styles["LandingPage-Section5-Column-Little-Title"]}>
+                                            Rising Star
+                                        </p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="400" className={styles["LandingPage-Section5-Column-Little-Desc"]}>
+                                            Lorem ipsum dolor sit amet, consectetur.
+                                        </p>
                                     </div>
                                 </Col>
                             </Row>
                             <Row className={styles["LandingPage-Section5-Column-Little-Row"]}>
                                 <Col className={styles["LandingPage-Section5-Column-Little"]}>
-                                    <div className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
+                                    <div data-aos="zoom-out-up" className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
                                         <Image className={styles["LandingPage-Section5-Column-Little-Icon"]} src={require("../../../public/images/Section5-No5.png")} />
                                     </div>
                                     <div
@@ -56,12 +102,16 @@ const LandingPageSection5 = () => {
                                             paddingLeft: "22px",
                                         }}
                                     >
-                                        <p className={styles["LandingPage-Section5-Column-Little-Title"]}>AA Hospitality</p>
-                                        <p className={styles["LandingPage-Section5-Column-Little-Desc"]}>Lorem ipsum dolor sit amet, consectetur.</p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="200" className={styles["LandingPage-Section5-Column-Little-Title"]}>
+                                            AA Hospitality
+                                        </p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="400" className={styles["LandingPage-Section5-Column-Little-Desc"]}>
+                                            Lorem ipsum dolor sit amet, consectetur.
+                                        </p>
                                     </div>
                                 </Col>
                                 <Col className={styles["LandingPage-Section5-Column-Little"]}>
-                                    <div className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
+                                    <div data-aos="zoom-out-up" className={styles["LandingPage-Section5-Column-Little-Icon-Container"]}>
                                         <Image className={styles["LandingPage-Section5-Column-Little-Icon"]} src={require("../../../public/images/Section5-No3.png")} />
                                     </div>
                                     <div
@@ -69,14 +119,18 @@ const LandingPageSection5 = () => {
                                             paddingLeft: "22px",
                                         }}
                                     >
-                                        <p className={styles["LandingPage-Section5-Column-Little-Title"]}>Outstanding Chef</p>
-                                        <p className={styles["LandingPage-Section5-Column-Little-Desc"]}>Lorem ipsum dolor sit amet, consectetur.</p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="200" className={styles["LandingPage-Section5-Column-Little-Title"]}>
+                                            Outstanding Chef
+                                        </p>
+                                        <p data-aos="zoom-out-up" data-aos-delay="400" className={styles["LandingPage-Section5-Column-Little-Desc"]}>
+                                            Lorem ipsum dolor sit amet, consectetur.
+                                        </p>
                                     </div>
                                 </Col>
                             </Row>
                         </div>
                     </Col>
-                    <Col className={styles["LandingPage-Section5-Column-Right"]} md={6} lg={5}>
+                    <Col ref={refImageContainer} className={styles["LandingPage-Section5-Column-Right"]} md={6} lg={5}>
                         <div className={styles["LandingPage-Section5-Image-Container"]}>
                             <Image className={styles["LandingPage-Section5-Image"]} src={require("../../../public/images/Section5-Right.png")} />
                         </div>
